52499ea068e4dcada3c2ed908335272b8e64b415
Hernandez <LuisJose.Hernandez@yrbrands.com>
2019-09-18 18:10:15 -0300

    tarea 4 at the office. included Bst, & handlebars

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
+var hbs = require('express-handlebars');
 var express = require("express");
 var app = express();
 
-app.get("/", function(req, res) {
-  res.send("Hello World!");
+app.get("/", function (req, res) {
+  res.render('home', { layout: 'default' });
 });
 
-app.listen(3000, function() {
+app.set('view engine', 'hbs');
+
+app.engine('hbs', hbs({
+  extname: 'hbs',
+  defaultView: 'default',
+  layoutsDir: __dirname + '/views/layouts/',
+  partialsDir: __dirname + '/views/utils/'
+}));
+
+app.listen(3000, function () {
   console.log("Example app listening on port 3000!");
 });
