3516ca5e5478de28be234ab449e7c6165ed7e9ed
Hernandez <LuisJose.Hernandez@yrbrands.com>
2019-09-30 17:35:44 -0300

    fix to css style sheet and 9º homework.

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(
   })
 );
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 app.get("/", function (req, res) {
   res.render('home', { layout: 'default' });
